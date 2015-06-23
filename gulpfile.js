be40021a04ffa47f2936a61ae03231cd287f459d
ygm <yuguangming125@126.com>
2015-06-23 19:57:44 +0800

    up

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,28 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var gulpif = require('gulp-if');
 
 var paths = {
 	controller : {
-		src : 'src/app/controller/**/*.js',
-		build : 'app/controller'
+		src : 'src/app/**',
+		build : 'app/'
 	}
 }
 
-gulp.task('buildController', function () {
+gulp.task('buildApp', function () {
     return gulp.src(paths.controller.src)
-        .pipe(babel({
-        	"blacklist": ["regenerator"]
-        }))
+        .pipe(gulpif(function(file){
+            if(/\.js$/.test(file.path)){
+                return true;
+            }
+        },babel({
+            "blacklist": ["regenerator"]
+        })))
         .pipe(gulp.dest(paths.controller.build));
 });
 
 gulp.task('watch', function() {
-  	gulp.watch(paths.controller.src, ['buildController']);
+  	gulp.watch(paths.controller.src, ['buildApp']);
 });
 
 gulp.task('default', ['watch']);
