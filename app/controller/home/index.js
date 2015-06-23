eab959c6e2838081b3cba47be2e91af65b49b649
ygm <yuguangming125@126.com>
2015-06-23 18:37:21 +0800

    up

--- app/controller/home/index.js
+++ app/controller/home/index.js
@@ -1,20 +1,20 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
 	value: true
 });
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; }
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; }
 
-var _base2 = require("./base");
+var _base2 = require('./base');
 
 var _base3 = _interopRequireDefault(_base2);
 
@@ -22,20 +22,27 @@ var index = (function (_base) {
 	function index() {
 		_classCallCheck(this, index);
 
-		_get(Object.getPrototypeOf(index.prototype), "constructor", this).apply(this, arguments);
+		_get(Object.getPrototypeOf(index.prototype), 'constructor', this).apply(this, arguments);
 	}
 
 	_inherits(index, _base);
 
 	_createClass(index, [{
-		key: "index",
+		key: 'index',
 		value: function* index(param) {
-			yield* this.display("index/index");
+
+			this.assign('mockdata', function (next) {
+				setTimeout(function () {
+					next(null, 'silkworm');
+				}, 500);
+			});
+
+			yield* this.display('index/index');
 		}
 	}]);
 
 	return index;
-})(_base3["default"]);
+})(_base3['default']);
 
-exports["default"] = index;
-module.exports = exports["default"];
+exports['default'] = index;
+module.exports = exports['default'];
--- src/app/controller/home/index.js
+++ src/app/controller/home/index.js
@@ -3,6 +3,13 @@ import base from "./base";
 class index extends base{
 	
 	*index(param){
+
+		this.assign( 'mockdata', function( next ) {
+            setTimeout(function(){
+            	next(null,'silkworm');
+            },500);
+        } );
+
 		yield* this.display('index/index');
 	}
 }
