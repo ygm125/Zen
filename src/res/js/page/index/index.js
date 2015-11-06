128a02ecc8048ce5f20c606585cb72b23da6a098
gm <MLS@2015-289deMacBook-Pro.local>
2015-11-06 16:10:40 +0800

    up

--- src/res/js/page/index/index.js
+++ src/res/js/page/index/index.js
@@ -1,4 +1,3 @@
 import { Menu } from './menu';
-import { Test } from './test';
 
 var start = new Menu(10, 10);
--- src/res/js/page/index/menu.js
+++ src/res/js/page/index/menu.js
@@ -1,4 +1,3 @@
-import { Test } from './test';
 export function Menu(x, y) {
   this.x = x;
   this.y = y;
